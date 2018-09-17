36f581b45eddc3609525b4644f324f8d12370ed3
Adriano Oliveira <adriano@wsmga-adriano.objectivemga>
2018-09-17 14:42:34 -0300

    Criando relatório

---
+++ src/main/java/com/drprado/pontointeligente/domain/reports/TestReport.java
@@ -0,0 +1,9 @@
+package com.drprado.pontointeligente.domain.reports;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+public class TestReport {
+    @PersistenceContext
+    private EntityManager em;
+}
